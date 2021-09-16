3cc10cd1036c3c647048a23cbb4d568611c50615
keerthana-tech <69675426+keerthana-tech@users.noreply.github.com>
2021-09-16 18:35:10 +0530

    Update 80_remove_dup_sortedarray II.py

--- 80_remove_dup_sortedarray II.py
+++ 80_remove_dup_sortedarray II.py
@@ -6,3 +6,15 @@ class Solution:
                 nums[j]=nums[i+1]
                 j+=1
         return nums
+
+    
+    class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        while i <len(nums)-2:
+            if nums[i]==nums[i+1]==nums[i+2]:
+                del nums[i]
+            else:
+                i+=1
+        return len(nums)
+            
