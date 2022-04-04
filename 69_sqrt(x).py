b4347623257feaaf9e7870a9c835001df025ca7e
keerthana-tech <69675426+keerthana-tech@users.noreply.github.com>
2022-04-04 21:30:07 +0530

    Create 69_sqrt(x).py

---
+++ 69_sqrt(x).py
@@ -0,0 +1,24 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0 or x==1:
+            return x
+        start=0
+        end=x
+        
+        while start <= end:
+            mid = (start+end)//2
+            mid_sq=mid*mid
+            
+            if mid_sq ==x:
+                return mid
+            elif mid_sq < x:
+                start=mid+1
+                ans=mid
+                
+                
+                
+                
+                
+            else:
+                end=mid-1
+        return ans
